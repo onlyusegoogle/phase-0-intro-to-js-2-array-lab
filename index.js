8da078809b97717ebf2165f320926707303344d7
mason white <masondwhite1@gmail.com>
2022-09-25 17:58:45 -0600

    commit tree bd2c1708614160b4a872f17e8de997b0f6d5fcd2

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+   cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+  cats.unshift(name)
+}
+
+function  destructivelyRemoveLastCat(){
+  cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+  cats.shift()
+}
+
+function appendCat(name){
+  const newLastCats = [...cats, name]
+  return newLastCats
+}
+
+function prependCat(name){
+  const newFirstCats = [name, ...cats]
+  return newFirstCats
+}
+
+function removeFirstCat () {
+  return cats.slice(1);
+}
+
+function removeLastCat(){
+  return cats.slice(0, cats.length - 1)
+}
+
+
+
+/*
+function removeLastCat(){
+  cats.slice(-1)
+}
+*/
